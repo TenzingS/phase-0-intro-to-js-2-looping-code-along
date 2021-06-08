213d6c38dc1661f57e72994ea2027becbbec4587
TenzingS <thesherpatenzing@gmail.com>
2021-06-08 01:03:28 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+function writeCards(listFriends, celebratedDay){
+    let thankCard = [];
+    for (let i=0; i<listFriends.length; i++){
+      thankCard.push(`Thank you, ${listFriends[i]}, for the wonderful ${celebratedDay} gift!`);
+    }
+
+    return thankCard;
+}
+
+writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+function countDown(){
+    let i = 10;
+    while(i >= 0){
+        console.log(i);
+        i--;
+    }
+}
+
+countDown(10);
+
+
